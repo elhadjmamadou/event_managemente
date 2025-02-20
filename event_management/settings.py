91a2b79d9941814d5f49c5ff7671ffc7f96ea962
Elhadj Mamadou Diallo <houssary356@gmail.com>
2025-02-20 13:21:44 +0000

    Ajout des config pour l'api

--- event_management/settings.py
+++ event_management/settings.py
@@ -150,3 +150,21 @@ CKEDITOR_CONFIGS = {
         'toolbar': None,
     },
 }
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+    ],
+}
